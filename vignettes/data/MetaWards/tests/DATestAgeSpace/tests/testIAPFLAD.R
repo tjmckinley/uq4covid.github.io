d6f3df641557772165b11e7252063d86f4a48400
TJ McKinley <t.mckinley@exeter.ac.uk>
2022-06-08 22:50:10 +0100

    Swapped testIAPF to use no seeds.

--- vignettes/data/MetaWards/tests/DATestAgeSpace/tests/testIAPFLAD.R
+++ vignettes/data/MetaWards/tests/DATestAgeSpace/tests/testIAPFLAD.R
@@ -31,8 +31,33 @@ contact <- read_csv("../inputs/POLYMOD_matrix.csv", col_names = FALSE) %>%
     as.matrix()
 
 ## read in initial conditions
-u1 <- readRDS("../outputs/u1.rds")
+#u1 <- readRDS("../outputs/u1.rds")
 u1_moves <- readRDS("../outputs/u1_moves.rds")
+
+## solution to round numbers preserving sum
+## adapted from:
+## https://stackoverflow.com/questions/32544646/round-vector-of-numerics-to-integer-while-preserving-their-sum
+smart_round <- function(x) {
+    y <- floor(x)
+    indices <- tail(order(x - y), round(sum(x)) - sum(y))
+    y[indices] <- y[indices] + 1
+    y
+}
+
+## add age probabilities
+ageProbs <- read_csv("../inputs/age_seeds.csv", col_names = FALSE)$X2
+
+## read in commuter data
+EW19 <- read_delim("../inputs/EW19.dat", delim = " ", col_names = FALSE)
+
+## expand to deal with age-classes
+u1 <- apply(EW19, 1, function(x, ageProbs) {
+        u <- matrix(0, 12, length(ageProbs))
+        u[1, ] <- smart_round(ageProbs * x[3])
+        list(u)
+    }, ageProbs = ageProbs) %>%
+    map(1) %>%
+    abind(along = 3)
 
 ## set seed for reproducibility
 set.seed(42)
