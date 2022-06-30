450a68a742184c14ee91e3195736c0135c284d7f
TJ McKinley <t.mckinley@exeter.ac.uk>
2022-06-30 12:03:49 +0100

    Fixed writeExt output error in BPF.

--- vignettes/data/MetaWards/tests/DATestAgeSpace/BPF.cpp
+++ vignettes/data/MetaWards/tests/DATestAgeSpace/BPF.cpp
@@ -1112,7 +1112,7 @@ List BPF_cpp (arma::vec pars, arma::mat C, arma::imat data, arma::uword nclasses
     double b, double a_dis, double b_dis, int saveAll, int writeExt, int PF, int ncores) {
     
     // set counters
-    arma::uword i, j, l, k, t;
+    arma::uword i, j, l, k, t = 0;
     
     // split u1 up into different LADs
     std::vector<arma::icube> u1(npart);
@@ -1750,7 +1750,7 @@ List BPF_cpp (arma::vec pars, arma::mat C, arma::imat data, arma::uword nclasses
                         file.open(file_name, std::ios::app);
                         for(l = 0; l < nlads; l++) {
                             for(arma::uword r = 0; r < 4; r++) {
-                                file << t << ", " << r << ", ";
+                                file << t + 1 << ", " << r << ", ";
                                 for(j = 0; j < nages; j++) {
                                     file << u1_night_reduced(r, j, l) << ", ";
                                 }
@@ -1808,7 +1808,7 @@ List BPF_cpp (arma::vec pars, arma::mat C, arma::imat data, arma::uword nclasses
                         file.open(file_name, std::ios::app);
                         for(l = 0; l < nlads; l++) {
                             for(arma::uword r = 0; r < (nclasses + 2); r++) {
-                                file << t << ", " << r << ", ";
+                                file << t + 1 << ", " << r << ", ";
                                 for(j = 0; j < nages; j++) {
                                     file << u1_night_full(r, j, l) << ", ";
                                 }
