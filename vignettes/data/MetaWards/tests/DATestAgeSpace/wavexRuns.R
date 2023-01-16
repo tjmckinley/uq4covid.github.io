44b075878e9b27705ba9a0cd40d8f0753f8abc98
TJ McKinley <t.mckinley@exeter.ac.uk>
2023-01-16 17:39:33 +0000

    Started to add age-specific MD.

--- vignettes/data/MetaWards/tests/DATestAgeSpace/wavexRuns.R
+++ vignettes/data/MetaWards/tests/DATestAgeSpace/wavexRuns.R
@@ -41,7 +41,7 @@ b1 <- as.numeric(fixedInputs[8])
 b2 <- as.numeric(fixedInputs[9])
 a_dis <- as.numeric(fixedInputs[10])
 b_dis <- as.numeric(fixedInputs[11])
-b_dis_london <- as.numeric(fixedInputs[12])
+b_dis_age8 <- as.numeric(fixedInputs[12])
 sigma2_lad <- as.numeric(fixedInputs[13])
 sigma2_age_region <- as.numeric(fixedInputs[14])
 sigma2_nhsregion <- as.numeric(fixedInputs[15])
@@ -119,14 +119,12 @@ age_lookup <- readRDS(paste0(outputs, "/age_lookup.rds"))
 u <- list(u1 = u1, u2 = u2, u1_moves = u1_moves, u2_moves = as.matrix(PM19))
 
 ## create model discrepancy matrices
-region_lookup <- readRDS("data/region_lookup.rds")
-london_FID <- lookup$FID[!is.na(lookup$FID_region) & lookup$FID_region == region_lookup$FID[region_lookup$RGN19NM == "London"]]
 a_dis <- a_dis * exp(-pars$MD_scale[hash] * (pars$MD_time[hash] - tstart:tstop) * ifelse(tstart:tstop < pars$MD_time[hash], 1, 0))
 a_dis <- array(rep(a_dis, nrow(age_lookup) * nrow(lookup)), c(tstop - tstart + 1, nrow(age_lookup), nrow(lookup)))
 b_dis <- b_dis * exp(-pars$MD_scale[hash] * (pars$MD_time[hash] - tstart:tstop) * ifelse(tstart:tstop < pars$MD_time[hash], 1, 0))
 b_dis <- array(rep(b_dis, nrow(age_lookup) * nrow(lookup)), c(tstop - tstart + 1, nrow(age_lookup), nrow(lookup)))
-b_dis_london <- b_dis_london * exp(-pars$MD_scale[hash] * (pars$MD_time[hash] - tstart:tstop) * ifelse(tstart:tstop < pars$MD_time[hash], 1, 0))
-b_dis[, , london_FID] <- rep(b_dis_london, length(london_FID))
+b_dis_age8 <- b_dis_age8 * exp(-pars$MD_scale[hash] * (pars$MD_time[hash] - tstart:tstop) * ifelse(tstart:tstop < pars$MD_time[hash], 1, 0))
+b_dis[, 8, ] <- rep(b_dis_age8, dim(b_dis)[3])
 
 ## run PF with some model discrepancy
 if(exists("hash")) {
