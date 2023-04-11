992d5a41cff326698e606e8213b2a1f95d2b54fa
TJ McKinley <t.mckinley@exeter.ac.uk>
2023-04-11 14:45:16 +0100

    Amended extractData to use new start date and extract top lads.

--- vignettes/data/MetaWards/tests/DATestAgeSpace/data/extractData.R
+++ vignettes/data/MetaWards/tests/DATestAgeSpace/data/extractData.R
@@ -4,8 +4,11 @@ library(lubridate)
 library(abind)
 
 ## set dates
-tstart <- dmy("15/01/2020")
+tstart <- dmy("15/02/2020")
 tstop <- dmy("23/03/2020")
+
+## set output name
+outnm <- "Feb"
 
 ## load data, extract relevant dates, fill missing
 ## values - assume deaths are zero when not observed
@@ -39,14 +42,24 @@ nhsregion_hosp <- readRDS("nhsregion_hosp.rds") %>%
 ## load lookup
 lookup <- readRDS("lookup.rds")
 lookup <- select(lookup, starts_with("FID"))
-    
 ## save outputs
-system("rm -r outputs")
-dir.create("outputs")
-saveRDS(death_lad, "outputs/cumDeath_lad.rds")
-saveRDS(death_region, "outputs/cumDeath_age_region.rds")
-saveRDS(nhsregion_cumadage, "outputs/cumHospAd_age_nhsregion.rds")
-saveRDS(nhsregion_hosp, "outputs/hosp_nhsregion.rds")
-saveRDS(lookup, "outputs/lookup.rds")
-system("cp age_lookup.rds outputs")
+system(paste0("rm -r outputs", outnm))
+dir.create(paste0("outputs", outnm))
+saveRDS(death_lad, paste0("outputs", outnm, "/cumDeath_lad.rds"))
+saveRDS(death_region, paste0("outputs", outnm, "/cumDeath_age_region.rds"))
+saveRDS(nhsregion_cumadage, paste0("outputs", outnm, "/cumHospAd_age_nhsregion.rds"))
+saveRDS(nhsregion_hosp, paste0("outputs", outnm, "/hosp_nhsregion.rds"))
+saveRDS(lookup, paste0("outputs", outnm, "/lookup.rds"))
+system(paste0("cp age_lookup.rds outputs", outnm))
+
+## write out top LADs
+pivot_longer(death_lad, !t, names_to = "lad") %>%
+    mutate(lad = as.numeric(gsub("deaths_", "", lad))) %>%
+    group_by(lad) %>%
+    summarise(value = sum(value), .groups = "drop") %>%
+    arrange(desc(value)) %>%
+    slice(1:10) %>%
+    select(lad) %>%
+    write_csv(file = paste0("outputs", outnm, "/lads_outputs", outnm, ".csv"), col_names = FALSE)
+    
 
