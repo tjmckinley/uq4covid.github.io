593e7ce81880a5a0fdf20b102cb3f364ad6a8319
TJ McKinley <t.mckinley@exeter.ac.uk>
2023-06-22 13:21:40 +0100

    Updated runPlotAgg_full.

--- vignettes/data/MetaWards/tests/DATestAgeSpace/JASMINcode/runPlotAgg_full.sh
+++ vignettes/data/MetaWards/tests/DATestAgeSpace/JASMINcode/runPlotAgg_full.sh
@@ -12,9 +12,9 @@ echo $jobname
 
 ## run R script
 cd ..
-cmd="R CMD BATCH --no-restore --no-save --slave '--args $2 $3 ${jobname}' JASMINcode/plotxAgg.R plot$2Agg_T${jobname}.Rout"
+cmd="R CMD BATCH --no-restore --no-save --slave '--args $2 $3 ${jobname}' JASMINcode/plotxAgg_full.R plot$2Agg_full_T${jobname}.Rout"
 eval $cmd
 
-cmd="mv plot$2Agg_T${jobname}.Rout wave$2"
+cmd="mv plot$2Agg_full_T${jobname}.Rout wave$2"
 eval $cmd
 
