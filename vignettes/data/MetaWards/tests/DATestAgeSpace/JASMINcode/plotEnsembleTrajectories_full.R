1ac87b03087513c92e911e4a9afd8f0787eb70fa
TJ McKinley <t.mckinley@exeter.ac.uk>
2023-07-06 10:05:08 +0100

    Updated plotEns_full.

--- vignettes/data/MetaWards/tests/DATestAgeSpace/JASMINcode/plotEnsembleTrajectories_full.R
+++ vignettes/data/MetaWards/tests/DATestAgeSpace/JASMINcode/plotEnsembleTrajectories_full.R
@@ -104,6 +104,14 @@ hosp <- readRDS(paste0("../", outputs, "/cumHosp_age_lad.rds")) %>%
     group_by(age, lad) %>%
     mutate(n = cumsum(n)) %>%
     ungroup()
+H <- readRDS(paste0("../", outputs, "/H_age_lad.rds")) %>%
+    filter(t <= tstop) %>%
+    pivot_longer(!t, values_to = "n") %>%
+    mutate(age = gsub('^(?:[^_]*_)(.*)', '\\1', name)) %>%
+    mutate(lad = gsub('^(?:[^_]*_)(.*)', '\\1', age)) %>%
+    mutate(age = gsub('(.*)_[0-9]*', '\\1', age)) %>%
+    mutate(across(c(age, lad), as.numeric)) %>%
+    select(!name)
 
 ## load in runs
 sims_md <- readRDS(paste0("../wave", wave, "/sumEns_natAgeDeathsHosp.rds")) %>%
@@ -152,6 +160,27 @@ p1[[3]] <- filter(sims_md, var == "hosp") %>%
         ggtitle("Observed cumulative hospitalisations (aggregated over LTLAs)")
     
 if(cont) p1[[3]] <- p1[[3]] + geom_vline(xintercept = tstart, linetype = "dashed")
+
+## plot hospital cases
+p1[[4]] <- filter(sims_md, var == "H") %>%
+    select(!var) %>%
+    pivot_wider(names_from = name, values_from = value) %>%
+    ggplot(aes(x = t)) +
+        geom_ribbon(aes(ymin = LCI, ymax = UCI), alpha = 0.5) +
+        geom_ribbon(aes(ymin = LQ, ymax = UQ), alpha = 0.5) +
+        geom_line(aes(y = Median)) +
+        geom_line(
+            aes(y = n), 
+            data = group_by(H, t, age) %>%
+                summarise(n = sum(n), .groups = "drop"),
+            col = "blue", linetype = "dashed"
+        ) +
+        facet_wrap(~age) +
+        xlab("Days") + 
+        ylab("Counts") +
+        ggtitle("Observed hospital cases (aggregated over LTLAs)")
+    
+if(cont) p1[[4]] <- p1[[4]] + geom_vline(xintercept = tstart, linetype = "dashed")
             
 ###############################################
 #####   combine plots and save outputs    #####
@@ -161,7 +190,7 @@ if(cont) p1[[3]] <- p1[[3]] + geom_vline(xintercept = tstart, linetype = "dashed
 saveRDS(p1, paste0("../wave", wave, "/plots.rds"))
 
 ## combine plots
-p1 <- (p1[[1]] + plot_spacer()) / (p1[[2]] + p1[[3]])
+p1 <- (p1[[1]] + p1[[2]]) / (p1[[3]] + p1[[4]])
 ggsave(paste0("../wave", wave, "/simsBPFEns.pdf"), p1, width = 15, height = 15)
 
 ###############################################
@@ -225,6 +254,7 @@ if(file.exists(paste0("../", outputs, "/lads_", outputs, ".txt"))) {
     ## load in data
     deaths <- filter(deaths, lad %in% lads)
     hosp <- filter(hosp, lad %in% lads)
+    H <- filter(H, lad %in% lads)
 
     ## load in runs
     sims_md <- readRDS(paste0("../wave", wave, "/sumEns_age_lads.rds")) %>%
@@ -265,12 +295,28 @@ if(file.exists(paste0("../", outputs, "/lads_", outputs, ".txt"))) {
             xlab("Days") + 
             ylab("Counts") +
             ggtitle(paste0("Observed hospitalisations in top ", length(lads), " LTLAs"))
+    p1[[3]] <- filter(sims_md, var == "H") %>%
+        ggplot(aes(x = t)) +
+            geom_ribbon(aes(ymin = LCI, ymax = UCI), colour = NA, alpha = 0.5) +
+            geom_ribbon(aes(ymin = LQ, ymax = UQ), colour = NA, alpha = 0.5) +
+            geom_line(aes(y = Median)) +
+            geom_line(
+                aes(y = n), 
+                data = H,
+                linetype = "dashed",
+                col = "blue"
+            ) +
+            facet_grid(lad ~ age, scales = "free") +
+            xlab("Days") + 
+            ylab("Counts") +
+            ggtitle(paste0("Observed hospital cases in top ", length(lads), " LTLAs"))
 
     if(cont) {
         p1[[1]] <- p1[[1]] + geom_vline(xintercept = tstart, linetype = "dashed")
         p1[[2]] <- p1[[2]] + geom_vline(xintercept = tstart, linetype = "dashed")
+        p1[[3]] <- p1[[3]] + geom_vline(xintercept = tstart, linetype = "dashed")
     }
-    p1 <- p1[[1]] + p1[[2]]
+    p1 <- (p1[[1]] + p1[[2]]) / (p1[[3]] + plot_spacer())
 
     ggsave(paste0("../wave", wave, "/simsTopLADsBPFEns.pdf"), p1, width = 25, height = 12)
 }
